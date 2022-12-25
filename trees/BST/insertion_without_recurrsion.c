93ba8fe73be1cdf7c83367a13ac45861bb36bc3a
vedika sontakke <vedikasontakke2003@gmail.com>
2022-12-25 17:32:56 -0300

    some changes

--- trees/BST/insertion_without_recurrsion.c
+++ trees/BST/insertion_without_recurrsion.c
@@ -6,41 +6,39 @@ struct node{
     struct node * left;
     struct node * right;
 };
-struct node *finalroot;
-struct node * insert(struct node * root , int x)
-{
-          printf("3\n");
 
-    struct node* temp = root;
-    struct node * newnode = (struct node *)malloc(sizeof(struct node *));
-    newnode->data = x;
-    newnode->left = newnode->right = NULL;
-    // tree is empty;
-    if(root == NULL)
-    {       
-        printf("c\n");
-        finalroot = newnode;
-        return newnode;
-    } 
-    else
-    {
-         while(root !=  NULL)
-        {  
-               if(x < root->data)
-               {
-                  temp = root;
-                  root = root->left;
-               }
-               else{
-                  temp = root;
-                  root = root->right;
-                }
-        }
-        if(x < temp->data) temp->left = newnode;
-       else temp->right = newnode;
-       printf("b\n");
-       return newnode;
-    }  
+struct node *finalroot;
+struct node * root ;
+
+void insert(int num)
+{
+   struct node * newnode = (struct node *)malloc(sizeof(struct node));
+   newnode->data = num;
+   newnode->left = newnode->right = NULL;
+
+   if(finalroot == NULL){
+      finalroot = newnode;
+      return 0;
+   } 
+   
+   struct node * curr = finalroot;
+   struct node * prev = finalroot;
+
+   while(curr != NULL)
+   {
+      if(num < curr->data)
+      {
+         prev = curr;
+         curr = curr->left;
+      }else if(num > curr->data)
+      {
+         prev = curr;
+         curr = curr->right;
+      }
+   }
+
+   if(num < prev->data) prev->left = newnode;
+   else prev->right = newnode;
 }
 
 void inorder(struct node* finalroot)
@@ -54,7 +52,6 @@ void inorder(struct node* finalroot)
 
 int main()
 {
-   struct node * root ;
    root = NULL;
    int size;
    printf("enter the number of element you want to insert :\n");
@@ -65,7 +62,7 @@ int main()
    for(int i=0; i<size ; i++)
    {
       printf("enter the %d element : ", i+1);
-      scanf("%d ", &element);
+      scanf("%d", &element);
       printf("a");
       if(root == NULL) {
                printf("d");
@@ -74,18 +71,18 @@ int main()
       }
       else {
          insert(root , element);
-      }
+      }   
    }
    
    printf("inorder traversal :");
    inorder(finalroot);
-/*
+
    printf("\nenter a elment which you want to insert :");
    scanf("%d" , &element);
    insert(root , element); 
 
    printf("\ninorder traversal after inseration :");
    inorder(finalroot);
-*/
+
  
 }
